43cc1afd5a7ec1d3b29502bd3a1d18f4a3d6ae86
Michal Moskal <michal@moskal.me>
2025-01-28 21:57:47 +0000

    basic gbnf->lark converter

--- sample_parser/lark.sh
+++ sample_parser/lark.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-    LARK=data/lark.lark
-fi
+case "$1" in
+    *.gbnf )
+        mkdir -p tmp
+        echo "Generating Lark grammar from GBNF $1 to tmp/gbnf.lark"
+        ../scripts/gbnf_to_lark.py "$1" > tmp/gbnf.lark
+        LARK=tmp/gbnf.lark
+        ;;
+    *.lark )
+        LARK="$1"
+        ;;
+    test )
+        ../scripts/gbnf_to_lark.py tmp/grammars/*.gbnf
+        cargo run --bin lark_test -- tmp/grammars/*.lark
+        exit $?
+        ;;
+    *)
+        echo "Usage: $0 <lark_file> [args...]"
+        exit 1
+        ;;
+esac
 
-cargo run --bin lark_test -- $LARK "$@"
+cargo run --bin lark_test -- $LARK
+
